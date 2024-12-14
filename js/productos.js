120cc576afd465d8bb9e0ded4035b6bf5779051a
miguelAvatr <macollantes@avatar-global.com>
2024-12-13 22:12:08 -0500

    listado de productos

---
+++ js/productos.js
@@ -0,0 +1,248 @@
+var productos = [
+    {
+      "codigo": "I-001",
+      "nombre": "Zapatillas Urbanas Puma Pre Escolar 397421 19 Carina 2.0 V Ps",
+      "precio": 179.00,
+      "categoria": "Infantil",
+      "marca": "Puma",
+      "urlImagen": "https://footloose.vtexassets.com/arquivos/ids/1529343-300-auto?v=638666822373970000&width=300&height=auto&aspect=true"
+    },
+    {
+      "codigo": "I-002",
+      "nombre": "Zapatillas Urbanas Puma Pre Escolar 396742 02 Rebound V6 Lo Ac",
+      "precio": 159.20,
+      "categoria": "Infantil",
+      "marca": "Puma",
+      "urlImagen": "https://footloose.vtexassets.com/arquivos/ids/1453951-300-auto?v=638392771810600000&width=300&height=auto&aspect=true"
+    },
+    {
+      "codigo": "I-003",
+      "nombre": "Zapatillas Puma Pre Escolar 192971 03 PUMA FUN RACER AC PS",
+      "precio": 125.90,
+      "categoria": "Infantil",
+      "marca": "Puma",
+      "urlImagen": "https://passarelape.vtexassets.com/arquivos/ids/1205930-300-auto?v=637699165343330000&width=300&height=auto&aspect=true"
+    },
+    {
+      "codigo": "I-004",
+      "nombre": "Zapatillas Urbanas Puma Pre Escolar 398726 01 Rebound V6 Mid Bouncy",
+      "precio": 199.00,
+      "categoria": "Infantil",
+      "marca": "Puma",
+      "urlImagen": "https://footloose.vtexassets.com/arquivos/ids/1502431-300-auto?v=638579978364600000&width=300&height=auto&aspect=true"
+    },
+    {
+      "codigo": "I-005",
+      "nombre": "Zapatillas Urbanas Adidas Pre Escolar H03863 Hoops 3.0 Cf C Cuero",
+      "precio": 209.00,
+      "categoria": "Infantil",
+      "marca": "Adidas",
+      "urlImagen": "https://footloose.vtexassets.com/arquivos/ids/1501407-300-auto?v=638574753887070000&width=300&height=auto&aspect=true"
+    },
+    {
+      "codigo": "I-006",
+      "nombre": "Zapatillas Adidas Pre Escolar Gw6494",
+      "precio": 159.00,
+      "categoria": "Infantil",
+      "marca": "Adidas",
+      "urlImagen": "https://footloose.vtexassets.com/arquivos/ids/1306562-300-auto?v=637945369848830000&width=300&height=auto&aspect=true"
+    },
+    {
+      "codigo": "I-007",
+      "nombre": "Zapatillas Urbanas Adidas Pre Escolar Ih8122 Advantage Base 2.0 Cf C Cuero",
+      "precio": 119.00,
+      "categoria": "Infantil",
+      "marca": "Adidas",
+      "urlImagen": "https://footloose.vtexassets.com/arquivos/ids/1501801-300-auto?v=638574754846470000&width=300&height=auto&aspect=true"
+    },
+    {
+      "codigo": "I-008",
+      "nombre": "Zapatillas De Fútbol Adidas Pre Escolar Gy5780 Goletto Viii Tf J",
+      "precio": 104.90,
+      "categoria": "Infantil",
+      "marca": "Adidas",
+      "urlImagen": "https://footloose.vtexassets.com/arquivos/ids/1493764-300-auto?v=638541806098300000&width=300&height=auto&aspect=true"
+    },
+    {
+        "codigo": "M-001",
+        "nombre": "Zapatillas Urbanas Puma Mujeres 392290 24 Caven 2.0",
+        "precio": 229.00,
+        "categoria": "Mujer",
+        "marca": "Puma",
+        "urlImagen": "https://passarelape.vtexassets.com/arquivos/ids/1529392-300-auto?v=638666945096470000&width=300&height=auto&aspect=true"
+    },
+    {
+        "codigo": "M-002",
+        "nombre": "Zapatillas Urbanas Puma Mujeres 397567 01 Rebound V6 Low Coquette",
+        "precio": 229.00,
+        "categoria": "Mujer",
+        "marca": "Puma",
+        "urlImagen": "https://footloose.vtexassets.com/arquivos/ids/1518135-300-auto?v=638629598286870000&width=300&height=auto&aspect=true"
+    },
+    {
+        "codigo": "M-003",
+        "nombre": "Zapatillas Running Puma Mujeres 379478 15 Retaliate 3",
+        "precio": 229.00,
+        "categoria": "Mujer",
+        "marca": "Puma",
+        "urlImagen": "https://passarelape.vtexassets.com/arquivos/ids/1502451-300-auto?v=638579978435430000&width=300&height=auto&aspect=true"
+    },
+    {
+        "codigo": "M-004",
+        "nombre": "Zapatillas Urbanas Puma Mujeres 398247 02 Cali Canvas Wns",
+        "precio": 189.00,
+        "categoria": "Mujer",
+        "marca": "Puma",
+        "urlImagen": "https://footloose.vtexassets.com/arquivos/ids/1498234-300-auto?v=638556204549730000&width=300&height=auto&aspect=true"
+    },
+    {
+        "codigo": "M-005",
+        "nombre": "Zapatillas Deportivas Adidas Mujeres Ie0527 Runfalcon 5 W",
+        "precio": 139.00,
+        "categoria": "Mujer",
+        "marca": "Adidas",
+        "urlImagen": "https://footloose.vtexassets.com/arquivos/ids/1525775-300-auto?v=638654253498530000&width=300&height=auto&aspect=true"
+    },
+    {
+        "codigo": "M-006",
+        "nombre": "Zapatillas Urbanas Adidas Mujeres If8555 Advantage Base",
+        "precio": 139.90,
+        "categoria": "Mujer",
+        "marca": "Adidas",
+        "urlImagen": "https://footloose.vtexassets.com/arquivos/ids/1503742-300-auto?v=638581422077100000&width=300&height=auto&aspect=true"
+    },
+    {
+        "codigo": "M-007",
+        "nombre": "Zapatillas Running Adidas Mujeres If9342 Switch Move W",
+        "precio": 142.90,
+        "categoria": "Mujer",
+        "marca": "Adidas",
+        "urlImagen": "https://footloose.vtexassets.com/arquivos/ids/1493907-300-auto?v=638543339314100000&width=300&height=auto&aspect=true"
+    },
+    {
+        "codigo": "M-008",
+        "nombre": "Zapatillas Outdoor Adidas Mujeres Hq0935 Terrex Eastrail 2 W",
+        "precio": 279.00,
+        "categoria": "Mujer",
+        "marca": "Adidas",
+        "urlImagen": "https://footloose.vtexassets.com/arquivos/ids/1484817-300-auto?v=638503508897500000&width=300&height=auto&aspect=true"
+    },
+    {
+        "codigo": "H-001",
+        "nombre": "Zapatillas Urbanas Adidas Hombres Ih4804 Grand Court 2.0",
+        "precio": 249.00,
+        "categoria": "Hombre",
+        "marca": "Adidas",
+        "urlImagen": "https://passarelape.vtexassets.com/arquivos/ids/1526139-1200-auto?v=638654269938500000&width=1200&height=auto&aspect=true"
+    },
+    {
+        "codigo": "H-002",
+        "nombre": "Zapatillas Urbanas Puma Hombres 395104 08 Club 5V5 Sd Cuero",
+        "precio": 249.00,
+        "categoria": "Hombre",
+        "marca": "Puma",
+        "urlImagen": "https://passarelape.vtexassets.com/arquivos/ids/1528795-1200-auto?v=638664991987400000&width=1200&height=auto&aspect=true"
+    },
+    {
+        "codigo": "H-003",
+        "nombre": "Zapatillas Urbanas Adidas Hombres If6097 Advantage",
+        "precio": 174.90,
+        "categoria": "Hombre",
+        "marca": "Adidas",
+        "urlImagen": "https://passarelape.vtexassets.com/arquivos/ids/1503516-1200-auto?v=638581421681600000&width=1200&height=auto&aspect=true"
+    },
+    {
+        "codigo": "H-004",
+        "nombre": "Zapatillas Urbanas Puma Hombres 372605 02 Up",
+        "precio": 199.00,
+        "categoria": "Hombre",
+        "marca": "Puma",
+        "urlImagen": "https://passarelape.vtexassets.com/arquivos/ids/1498994-1200-auto?v=638561452492170000&width=1200&height=auto&aspect=true"
+    },
+    {
+        "codigo": "H-005",
+        "nombre": "Zapatillas Urbanas Hombres 380067 09 Skyrocket Lite Alt",
+        "precio": 159.00,
+        "categoria": "Hombre",
+        "marca": "Puma",
+        "urlImagen": "https://passarelape.vtexassets.com/arquivos/ids/1489731-1200-auto?v=638519496595430000&width=1200&height=auto&aspect=true"
+    },
+    {
+        "codigo": "H-006",
+        "nombre": "Zapatillas Urbanas Adidas Hombres Ie1023 Break Start Mid",
+        "precio": 195.90,
+        "categoria": "Hombre",
+        "marca": "Adidas",
+        "urlImagen": "https://passarelape.vtexassets.com/arquivos/ids/1503605-1200-auto?v=638581421833870000&width=1200&height=auto&aspect=true"
+    },
+    {
+        "codigo": "H-007",
+        "nombre": "Zapatillas Urbanas Puma Hombres 392328 03 Rebound V6 Low",
+        "precio": 229.00,
+        "categoria": "Hombre",
+        "marca": "Puma",
+        "urlImagen": "https://passarelape.vtexassets.com/arquivos/ids/1518016-1200-auto?v=638629596165870000&width=1200&height=auto&aspect=true"
+    },
+    {
+        "codigo": "H-008",
+        "nombre": "Zapatillas Outdoor Adidas Hombres Ie0815 Terrex Ax2S",
+        "precio": 279.00,
+        "categoria": "Hombre",
+        "marca": "Adidas",
+        "urlImagen": "https://passarelape.vtexassets.com/arquivos/ids/1501564-1200-auto?v=638574754239770000&width=1200&height=auto&aspect=true"
+    },
+    {
+        "codigo": "H-009",
+        "nombre": "Zapatillas Running Puma Hombres 380067 04 Skyrocket Lite Alt",
+        "precio": 199.00,
+        "categoria": "Hombre",
+        "marca": "Puma",
+        "urlImagen": "https://passarelape.vtexassets.com/arquivos/ids/1499529-1200-auto?v=638561456346800000&width=1200&height=auto&aspect=true"
+    }
+];
+
+  
+function llenarProductos () {
+    let html = '';
+
+    if (productos.length > 0) {
+        // Empiezo a llenar los productos
+
+        // Inicio de etiqueta para listado
+        html = `<div class="products-list">`
+
+        for (let i = 0; i < productos.length; i++) {
+            html += `
+              <article class="product">
+                <div class="image-product">
+                  <img src="${productos[i].urlImagen}" alt="Imagen de ${productos[i].nombre}">
+                </div>
+
+                <div class="name-product">
+                  <span>${productos[i].nombre}</span>
+                </div>
+
+                <div class="product-section">
+                  <span>Precio: </span>
+                  <span class="price-text"> S/ ${productos[i].precio}</span>
+                </div>
+
+                <div class="product-section">
+                  <button class="btn-add-cart">Añadir al carro</button>
+                </div>
+              </article>
+            `;
+        }
+
+        // Fin de etiqueta para listado
+        html += `</div>`
+    } else {
+        html = 'No hay productos';
+    }
+
+    document.querySelector('.productos-wrapper').innerHTML = html;
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    llenarProductos();
+});
